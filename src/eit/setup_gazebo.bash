f6ad0a28cc207b5d9cc7140ad4715b0c5988e88a
Soren Madsen <somad17@student.sdu.dk>
2022-02-01 13:59:47 +0100

    wave git copy added

---
+++ src/eit/setup_gazebo.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/PX4-Autopilot
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/PX4-Autopilot/Tools/sitl_gazebo
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/master_thesis_new/src/eit_playground/models
+
+echo ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH
