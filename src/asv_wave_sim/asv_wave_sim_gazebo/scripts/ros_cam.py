98a06a081096f9c54484a9a3f52eec45ebd4e57e
Soren Madsen <somad17@student.sdu.dk>
2022-04-12 14:45:48 +0200

    update

--- src/asv_wave_sim/asv_wave_sim_gazebo/scripts/ros_cam.py
+++ src/asv_wave_sim/asv_wave_sim_gazebo/scripts/ros_cam.py
@@ -1,16 +1,10 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
 import cv2
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-rawCapture = PiRGBArray(camera)
-# allow the camera to warmup
-time.sleep(0.1)
-# grab an image from the camera
-camera.capture(rawCapture, format="bgr")
-image = rawCapture.array
-# display the image on screen and wait for a keypress
-cv2.imwrite("Image", image)
-print("saving img")
-cv2.waitKey(0)
+
+cap = cv2.VideoCapture(0)
+
+# Capture frame
+ret, frame = cap.read()
+if ret:
+	cv2.imwrite('image.jpg', frame)
+
+cap.release()
