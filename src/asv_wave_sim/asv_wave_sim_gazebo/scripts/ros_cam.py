3d18f796be1adda57eda8064b2503fb63c2b47cf
Soren Madsen <somad17@student.sdu.dk>
2022-04-12 14:19:16 +0200

    update

--- src/asv_wave_sim/asv_wave_sim_gazebo/scripts/ros_cam.py
+++ src/asv_wave_sim/asv_wave_sim_gazebo/scripts/ros_cam.py
@@ -1,54 +1,16 @@
-#!/usr/bin/env python3
-#import rospy # Python library for ROS
-#from sensor_msgs.msg import Image # Image is the message type
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
-
-class CameraPublisher(object):
-    def __init__(self):
-        #self.setup_cam()
-        self.loop()
-
-    def setup_cam(self):
-        """
-        Publish the image from raspberry pi camera v2
-        """
-
-        # Tells rospy the name of the node.
-        #rospy.init_node('camera_publisher', anonymous=True)
-
-        # Publishing to the "video_frames" topic using the message type Image
-        #self.pub = rospy.Publisher('/video_frames', Image, queue_size=10)
-
-    def loop(self):
-        # go through the loop 15 times per second
-        #rate = rospy.Rate(15)
-
-        # Create a VideoCapture object The argument '0' gets the default webcam.
-        cap = cv2.VideoCapture(-1)
-
-
-            # Capture frame-by-frame
-            # This method returns True/False as well as the video frame.
-        ret, frame = cap.read()
-            # Print debugging information to the terminal
-            #rospy.loginfo('publishing video frame')
-
-            # converts an OpenCV image to a ROS image message
-            #img_msg = br.cv2_to_imgmsg(frame, encoding='rgb8')
-
-            # Publish the image.
-            #self.pub.publish(img_msg)
-        cv2.imwrite("picam.jpg", frame)
-        print("Saving img")
-
-            # Sleep just enough to maintain the desired rate
-            #rate.sleep()
-        cap.release()
-        cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    
-    CameraPublisher()
-    #except rospy.ROSInterruptException:
-        #print("failed")
-        #pass
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+# allow the camera to warmup
+time.sleep(0.1)
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+# display the image on screen and wait for a keypress
+cv2.imwrite("Image", image)
+print("saving img")
+cv2.waitKey(0)
