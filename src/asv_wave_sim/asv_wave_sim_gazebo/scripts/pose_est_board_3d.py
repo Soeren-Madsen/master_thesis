9459559e3aeb4e1c5812d9f0992f00e3a52de025
Raspberry <soren.madsen97@gmail.com>
2022-04-13 09:05:51 +0000

    Update pose est

--- src/asv_wave_sim/asv_wave_sim_gazebo/scripts/pose_est_board_3d.py
+++ src/asv_wave_sim/asv_wave_sim_gazebo/scripts/pose_est_board_3d.py
@@ -97,33 +97,33 @@ class Aruco_pose():
                     # Store the rotation information
                     rotation_matrix = np.eye(4)
                     try:
-                        print("test")
+                        #print("test")
                         rotation_matrix[0:3, 0:3] = cv2.Rodrigues(rvecs)[0]
                         r = R.from_matrix(rotation_matrix[0:3, 0:3])
                         quat = r.as_quat()   
-                        print("test2")
+                        #print("test2")
                         # Quaternion format     
                         transform_rotation_x = quat[0] 
                         transform_rotation_y = quat[1] 
                         transform_rotation_z = quat[2] 
                         transform_rotation_w = quat[3]
-                        print("test3") 
+                        #print("test3") 
                         # Euler angle format in radians
                         roll_x, pitch_y, yaw_z = self.euler_from_quaternion(transform_rotation_x, 
                                                                         transform_rotation_y, 
                                                                         transform_rotation_z, 
                                                                         transform_rotation_w)
-                        print("test4")
+                        #print("test4")
                         roll_x = math.degrees(roll_x)
-                        print("test5")
+                        #print("test5")
                         pitch_y = math.degrees(pitch_y)
-                        print("test6")
+                        #print("test6")
                         yaw_z = math.degrees(yaw_z)
-                        print("test7")
+                        #print("test7")
                         #print("transform_translation_x: {}".format(transform_translation_x))
                         #print("transform_translation_y: {}".format(transform_translation_y))
                         print("transform_translation_z: {}".format(self.transform_translation_z))
-                        print("test")
+                        #print("test")
                         print("roll_x: {}".format(roll_x))
                         print("pitch_y: {}".format(pitch_y))
                         print("yaw_z: {}".format(yaw_z))
@@ -153,9 +153,9 @@ def main():
     
         ret, frame = cap.read()
         aru.calc_euler(frame)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        #cv2.imshow('frame', frame)
+        #if cv2.waitKey(1) & 0xFF == ord('q'):
+            #break
 
     
     # Close down the video stream
